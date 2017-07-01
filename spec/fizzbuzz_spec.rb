4dd20ce3acdc6d701439e010f9c69ace7e56a356
chuk-chuk <yulia.v.litvin@gmail.com>
2017-07-01 14:10:32 +0100

    added fuzzbuzz_rspec.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,27 +1,29 @@
 require 'fizzbuzz'
 
-describe "fizzbuzz" do
-  it "returns 'fizz' if self is a multiple of 3" do
-    expect(3.fizzbuzz).to eq "fizz"
-    expect(6.fizzbuzz).to eq "fizz"
-    expect(27.fizzbuzz).to eq "fizz"
-  end
+describe Integer do
+  context "when testing the Integer class" do
+    it "returns 'fizz' if self is a multiple of 3" do
+      expect(3.fizzbuzz).to eq "fizz"
+      #expect(6.fizzbuzz).to eq "fizz"
+     # expect(27.fizzbuzz).to eq "fizz"
+    end
 
-  it "returns 'buzz' if self is a multiple of 5" do
-    expect(5.fizzbuzz).to eq "buzz"
-    expect(25.fizzbuzz).to eq "buzz"
-    expect(40.fizzbuzz).to eq "buzz"
-  end
+    it "returns 'buzz' if self is a multiple of 5" do
+      expect(5.fizzbuzz).to eq "buzz"
+     # expect(25.fizzbuzz).to eq "buzz"
+     # expect(40.fizzbuzz).to eq "buzz"
+    end
 
-  it "returns 'fizzbuzz' when self is a multople of 3 & 5" do
-    expect(15.fizzbuzz).to eq "fizzbuzz"
-    expect(45.fizzbuzz).to eq "fizzbuzz"
-    expect(90.fizzbuzz).to eq "fizzbuzz"
-  end
+    it "returns 'fizzbuzz' when self is a multople of 3 & 5" do
+      expect(15.fizzbuzz).to eq "fizzbuzz"
+     # expect(45.fizzbuzz).to eq "fizzbuzz"
+      #expect(90.fizzbuzz).to eq "fizzbuzz"
+    end
 
-  it "returns 4 if self equals 4" do
-    expect(4.fizzbuzz).to eq 4
-    expect(7.fizzbuzz).to eq 7
-    expect(61.fizzbuzz).to eq 61
+    it "returns 4 if self equals 4" do
+      expect(4.fizzbuzz).to eq 4
+     # expect(7.fizzbuzz).to eq 7
+     # expect(61.fizzbuzz).to eq 61
+    end
   end
 end
